0e6ae240958bfffc6adadc8f49eb12fedb7e2c8d
Allie Zhao <allie.k.zhao@gmail.com>
2022-11-16 13:00:20 -0800

    content page now redirects to login/up page if not signed in

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -2,7 +2,6 @@ import { checkAuth, logout } from '../fetch-utils.js';
 
 const logoutButton = document.getElementById('logout');
 
-// use checkAuth function to redirect is user not authenticated
 // add event listener to the logout button and call logout
 
 logoutButton.addEventListener('click', async () => {
@@ -11,3 +10,6 @@ logoutButton.addEventListener('click', async () => {
 
     console.log("if you're seeing this the logout didnt work");
 });
+
+// use checkAuth function to redirect is user not authenticated
+checkAuth();
