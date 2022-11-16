c79f06b90dbbc814e19faa1c0b55ae1ff6169171
Allie Zhao <allie.k.zhao@gmail.com>
2022-11-16 12:30:26 -0800

    you can now make a new account and get auto logged in after you do so

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+import { signupUser, redirectIfLoggedIn } from './fetch-utils.js';
+
 const signInForm = document.getElementById('sign-in');
 
 const signUpForm = document.getElementById('sign-up');
+const signUpEmailInput = document.getElementById('sign-up-email');
+const signUpPasswordInput = document.getElementById('sign-up-password');
 
 // Wire up sign in and sign up forms to supabase
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    // should return user data if successful signup
+    const user = await signupUser(signUpEmailInput.value, signUpPasswordInput.value);
+    console.log(user);
+
+    // check if logged in after signing up, redirect to content page if so
+    if (user) {
+        //     redirectIfLoggedIn();
+    } else {
+        console.log(user);
+    }
+});
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,19 +1,32 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://sagftzksyhlnwvwqislb.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhZ2Z0emtzeWhsbnd2d3Fpc2xiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgxMzcsImV4cCI6MTk4MzY4NDEzN30.YKP0xHsE1Oo9sKmU0QIHWN5YTQFCCQYfW4p1zJ1iOtw';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+// returns a user, if there is one
 export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+// signs in new user, puts auth token in local browser storage
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+    return response.user;
+}
 
+// signs in existing user, puts auth token in local browser storage
 export async function signInUser(email, password) {}
 
+// call on page load: when called, redirects to login page if not logged in
 export async function checkAuth() {}
 
-export async function redirectIfLoggedIn() {}
+// called on page load, redirects away from page if user is already logged in
+export async function redirectIfLoggedIn() {
+    if (getUser()) {
+        location.replace('./other-page/');
+    }
+}
 
 export async function logout() {}
